ecf0da671a96d41bda4cf16df8969fd384b6a17f
Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>
2022-12-18 04:00:02 +0530

    Optimize scala solution for 2022 day 05
    Signed-off-by: Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>

--- scala-aoc/src/main/scala/io/c6/aoc/y2022/Day05.scala
+++ scala-aoc/src/main/scala/io/c6/aoc/y2022/Day05.scala
@@ -1,73 +1,73 @@
 package io.c6.aoc.y2022
 
 import io.c6.aoc.BaseSolution
+import io.c6.aoc.BaseSolution.*
 import io.c6.aoc.Day.*
 import io.c6.aoc.InputType.*
 import io.c6.aoc.Year.*
 
 import scala.collection.mutable
 
-//noinspection DuplicatedCode
 object Day05 extends BaseSolution:
-  override protected def part1InputFileName: String = BaseSolution.getInputFileName(_2022, _05, A1)
+  override protected def part1InputFileName: String = getInputFileName(_2022, _05, A1)
 
-  private type Stack = mutable.Stack[Char]
+  private type Crate = Char
+  private type Stack = mutable.Stack[Crate]
+
+  private enum Crane:
+    case CrateMover9000, CrateMover9001
+
+  private enum Instruction:
+    case LoadInstruction, MoveInstruction, SomethingElse
+
+  import Crane.*
+  import Instruction.*
 
   extension (stack: Stack)
-    private def load(elem: Char): Stack = if (elem == ' ') stack else stack.append(elem)
+    private def load(crate: Crate): Stack = if (crate.isUpper) stack.append(crate) else stack
 
-  private def nothing: Unit = ()
+  extension (crane: Crane)
+    private def move(count: Int, from: Stack, to: Stack): Unit = crane match
+      case CrateMover9000 => (1 to count).foreach(_ => to.push(from.pop()))
+      case CrateMover9001 => to.pushAll((1 to count).map(_ => from.pop()).reverse)
+
+  // distance between crates in the load instruction
   private val gap = 4
 
-  private def loadStacks(stacks: Array[Stack], instruction: String): Unit =
-    val len = instruction.length
-    var m = 0
-    while (m < len)
-      stacks(m / gap).load(instruction(m + 1))
-      m += gap
-
-  private def rearrangeStacks(stacks: Array[Stack], instruction: String): Unit =
-    val (steps, from, to) = instruction.split(' ') match
+  private def parseMoveInstruction(line: String): (Int, Int, Int) =
+    line.split(' ') match
       case Array(_, a, _, b, _, c) => (a.toInt, b.toInt - 1, c.toInt - 1)
-    val creates = (1 to steps).map { _ => stacks(from).pop() }
-    stacks(to).pushAll(creates)
 
-  private def rearrangeStacksPart2(stacks: Array[Stack], instruction: String): Unit =
-    val (steps, from, to) = instruction.split(' ') match
-      case Array(_, a, _, b, _, c) => (a.toInt, b.toInt - 1, c.toInt - 1)
-    val creates = (1 to steps).map { _ => stacks(from).pop() }
-    stacks(to).pushAll(creates.reverse)
+  private def parseLoadInstruction(line: String): Seq[(Int, Crate)] =
+    (0 until line.length by gap).map { i => (i / gap) -> line(i + 1) }
+
+  private def toInstruction(line: String): Instruction =
+    Option(line).map(_.trim).filter(_.nonEmpty).getOrElse(" ").charAt(0) match
+      case '[' => LoadInstruction
+      case 'm' => MoveInstruction
+      case _ => SomethingElse
+
+  private def processInstruction(stacks: Array[Stack], crane: Crane)(line: String): Unit =
+    toInstruction(line) match
+      case LoadInstruction =>
+        parseLoadInstruction(line).foreach { case id -> crate =>
+          stacks(id).load(crate)
+        }
+      case MoveInstruction =>
+        val (count, from, to) = parseMoveInstruction(line)
+        crane.move(count, stacks(from), stacks(to))
+      case SomethingElse => ()
 
   override protected def part1Solution: Seq[String] => Unit = { input =>
-    val len = input.head.length
-    val stacks = Array.fill((len + 1) / gap)(new Stack)
-    input.foreach { line =>
-      val trimmedLine = line.trim
-      if (trimmedLine.startsWith("["))
-        loadStacks(stacks, line)
-      else if (trimmedLine.startsWith("move"))
-        rearrangeStacks(stacks, line)
-      else
-        nothing
-    }
-//    stacks.foreach(println)
+    val stacks = Array.fill((input.head.length + 1) / gap)(new Stack)
+    input.foreach(processInstruction(stacks, CrateMover9000))
     val result = stacks.map(_.top).mkString
     println(s"Result for part 1: $result")
   }
 
   override protected def part2Solution: Seq[String] => Unit = { input =>
-    val len = input.head.length
-    val stacks = Array.fill((len + 1) / gap)(new Stack)
-    input.foreach { line =>
-      val trimmedLine = line.trim
-      if (trimmedLine.startsWith("["))
-        loadStacks(stacks, line)
-      else if (trimmedLine.startsWith("move"))
-        rearrangeStacksPart2(stacks, line)
-      else
-        nothing
-    }
-//    stacks.foreach(println)
+    val stacks = Array.fill((input.head.length + 1) / gap)(new Stack)
+    input.foreach(processInstruction(stacks, CrateMover9001))
     val result = stacks.map(_.top).mkString
     println(s"Result for part 2: $result")
   }
