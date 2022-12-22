1a8046e004c89b8baf59ea658404da2dbcf5dd56
Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>
2022-12-22 17:54:33 +0530

    Add placeholder files for 2022
    Signed-off-by: Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>

---
+++ scala-aoc/src/main/scala/io/c6/aoc/y2022/Day11.scala
@@ -0,0 +1,15 @@
+package io.c6.aoc.y2022
+
+import io.c6.aoc.util.BaseSolution
+import io.c6.aoc.util.Day.*
+import io.c6.aoc.util.InputType.*
+import io.c6.aoc.util.Year.*
+
+@main def _2022_11: Unit = Day11()
+
+object Day11 extends BaseSolution(_2022, _11):
+  protected def part1Solution: Seq[String] => Unit = input =>
+    println("Part 1 has input size: " + input.length)
+
+  protected def part2Solution: Seq[String] => Unit = input =>
+    println("Part 2 has input size: " + input.length)
---
+++ scala-aoc/src/main/scala/io/c6/aoc/y2022/Day24.scala
@@ -0,0 +1,15 @@
+package io.c6.aoc.y2022
+
+import io.c6.aoc.util.BaseSolution
+import io.c6.aoc.util.Day.*
+import io.c6.aoc.util.InputType.*
+import io.c6.aoc.util.Year.*
+
+@main def _2022_24: Unit = Day24()
+
+object Day24 extends BaseSolution(_2022, _24):
+  protected def part1Solution: Seq[String] => Unit = input =>
+    println("Part 1 has input size: " + input.length)
+
+  protected def part2Solution: Seq[String] => Unit = input =>
+    println("Part 2 has input size: " + input.length)
