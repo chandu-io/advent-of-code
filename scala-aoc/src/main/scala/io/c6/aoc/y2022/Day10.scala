e0d2d485b9e6d3aa07b6a958eb00064b0f556611
Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>
2022-12-27 02:35:23 +0530

    Add scala solution for 2022 day 10
    Signed-off-by: Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>

--- scala-aoc/src/main/scala/io/c6/aoc/y2022/Day10.scala
+++ scala-aoc/src/main/scala/io/c6/aoc/y2022/Day10.scala
@@ -5,11 +5,28 @@ import io.c6.aoc.util.Day.*
 import io.c6.aoc.util.InputType.*
 import io.c6.aoc.util.Year.*
 
-@main def _2022_10: Unit = Day10()
+@main def _2022_10: Unit = Day10(Actual)
 
 object Day10 extends BaseSolution(_2022, _10):
+  private val width = 40
+  private val height = 6
+  private val signalStrengthCycles = LazyList.from(20, width).take(height)
+  private val cycles = 1 to (width * height)
+
+  private def computeRegisterRunningValues(instructions: Seq[String]) =
+    instructions.flatMap { instruction =>
+      if instruction == "noop" then Seq(0) else Seq(0, instruction.substring(5).toInt)
+    }.scanLeft(1)(_ + _)
+
   protected def part1Solution: Seq[String] => Unit = input =>
-    println("Part 1 has input size: " + input.length)
+    val registerValues = computeRegisterRunningValues(input)
+    val result = signalStrengthCycles.map(cycle => cycle * registerValues(cycle - 1)).sum
+    println(s"Result for part 1: $result")
 
   protected def part2Solution: Seq[String] => Unit = input =>
-    println("Part 2 has input size: " + input.length)
+    val registerValues = computeRegisterRunningValues(input)
+    val result = cycles.map { cycle =>
+      val pixel = (cycle - 1) % width
+      if Set(pixel - 1, pixel, pixel + 1).contains(registerValues(cycle - 1)) then '#' else '.'
+    }.grouped(width).map(_.mkString).mkString(System.lineSeparator())
+    println(s"Result for part 2: ${System.lineSeparator()}$result")
