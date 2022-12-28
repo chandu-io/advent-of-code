101ab7a50b0166ff1f149ce06244a6d98c4a95e9
Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>
2022-12-28 20:59:44 +0530

    Add scala solution for 2022 day 13
    Signed-off-by: Chandrasekhar Thotakura <chandu-io@users.noreply.github.com>

--- scala-aoc/src/main/scala/io/c6/aoc/util/Utils.scala
+++ scala-aoc/src/main/scala/io/c6/aoc/util/Utils.scala
@@ -1,7 +1,10 @@
 package io.c6.aoc.util
 
+import scala.annotation.tailrec
 import scala.concurrent.duration.{Duration, NANOSECONDS}
+import scala.collection.mutable
 
+//noinspection ScalaWeakerAccess, ScalaUnusedSymbol
 object Utils:
   def time[T](operation: => T): T =
     val startTime = System.nanoTime()
@@ -9,3 +12,25 @@ object Utils:
     val elapsedDuration = Duration(System.nanoTime() - startTime, NANOSECONDS)
     println(s"Time taken: ${elapsedDuration.toMillis} milliseconds")
     result
+
+  object SeqExt:
+    extension[T] (seq: Seq[T])
+      def split(separator: T): Seq[Seq[T]] =
+        splitBy(_ == separator)
+
+      def splitBy(predicate: T => Boolean): Seq[Seq[T]] =
+        val parent = mutable.ListBuffer[Seq[T]]()
+        val child = mutable.ListBuffer[T]()
+        seq.foreach { value =>
+          if predicate(value) then
+            parent.append(child.toSeq)
+            child.clear
+          else
+            child.append(value)
+        }
+        parent.append(child.toSeq)
+        parent.toSeq
+
+      def toPairOption: Option[(T, T)] =
+        val pairSeq = seq.take(2)
+        if pairSeq.length == 2 then Some(pairSeq.head -> pairSeq.last) else None
--- scala-aoc/src/main/scala/io/c6/aoc/y2022/Day13.scala
+++ scala-aoc/src/main/scala/io/c6/aoc/y2022/Day13.scala
@@ -5,11 +5,78 @@ import io.c6.aoc.util.Day.*
 import io.c6.aoc.util.InputType.*
 import io.c6.aoc.util.Year.*
 
+import scala.collection.mutable
+
 @main def _2022_13: Unit = Day13()
 
 object Day13 extends BaseSolution(_2022, _13):
+
+  private val start = "["
+  private val end = "]"
+  private val separator = ","
+  private val empty = ""
+  private val space = " "
+  private type Signal = Packet | Data
+
+  private case class Packet(signals: Signal*) extends Ordered[Packet]:
+    private def appended(signal: Signal): Packet = Packet(signals :+ signal: _*)
+
+    def compare(other: Packet): Int =
+      val result = signals.zip(other.signals).map {
+        case (Data(x), Data(y)) => x.compare(y)
+        case (packet: Packet, data: Data) => packet.compare(Packet(data))
+        case (data: Data, packet: Packet) => Packet(data).compare(packet)
+        case (packetLeft: Packet, packetRight: Packet) => packetLeft.compare(packetRight)
+        case _ => 0
+      }
+      val (m, n) = signals.length -> other.signals.length
+      result.dropWhile(_ == 0).headOption.getOrElse(m.compare(n))
+
+    override def toString: String = signals.mkString(start, separator, end)
+
+  private case class Data(value: Int):
+    override def toString: String = value.toString
+
+  private object Packet:
+    def apply(str: String): Packet =
+      if str.isEmpty || str.head != start.head || str.last != end.head then return Packet()
+      val stack = mutable.Stack[Packet]()
+      var num = empty
+      var packet = Packet()
+      str.replace(space, empty).foreach { c =>
+        if c == start.head then
+          stack.push(Packet())
+        else if c.isDigit then
+          num += c
+        else if c == end.head then
+          var popped = stack.pop
+          if num.nonEmpty then
+            popped = popped.appended(Data(num.toInt))
+            num = empty
+          if stack.isEmpty then
+            packet = popped
+          else
+            stack.push(stack.pop.appended(popped))
+        else if c == separator.head then
+          if num.nonEmpty then
+            stack.push(stack.pop.appended(Data(num.toInt)))
+            num = empty
+      }
+      packet
+
   protected def part1Solution: Seq[String] => Unit = input =>
-    println("Part 1 has input size: " + input.length)
+    import io.c6.aoc.util.Utils.SeqExt.*
+    val packetPairs = input.split(empty)
+    val result = packetPairs.zipWithIndex
+      .filter { (seq, _) => seq.map(Packet(_)).toPairOption.exists(_ < _) }
+      .map((_, idx) => idx + 1).sum
+    println(s"Result for part 1: $result")
 
   protected def part2Solution: Seq[String] => Unit = input =>
-    println("Part 2 has input size: " + input.length)
+    implicit val ordering: Ordering[String] = Packet(_) compare Packet(_)
+    val dividers = Seq("[[2]]", "[[6]]")
+    val unorderedPackets = input.filter(_.nonEmpty) ++ dividers
+    val result = unorderedPackets.sorted.zipWithIndex
+      .filter { (p, _) => dividers.contains(p) }
+      .map((_, idx) => idx + 1).product
+    println(s"Result for part 2: $result")
